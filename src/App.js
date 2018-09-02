883b9ca4b9cfaa448f44f2db213017402835dfb0
qiuyuanyin <acey0622@gmail.com>
2018-09-02 23:40:17 +1000

    [ADD] styled Thumbnail component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Thumbnail from './Thumbnail';
 
+// react root component
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Thumbnail> something interesting</Thumbnail>
+        <Thumbnail />
       </div>
     );
   }
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -3,28 +3,58 @@
  */
 import styled from 'styled-components';
 import React, { Component } from 'react';
+import backgroundImg from './assets/background.jpg';
 
-const mainColor = 'indianred';
+// several components to render the seven box
 
-const Background = styled.img`
-  flex-grow: 1;
-  width: 300px;
-  height: 250px;
-  padding: 5px;
-  margin: 15px;
-  border: 1px solid ${mainColor};
-  border-radius: 8px;
+const Background = styled.div`
+	width: 500px;
+  height: 280px;
+  margin: 2.5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  background: url(${backgroundImg}); 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  @media (max-width: 500px) {
+    width: 95%;
+    height: 50vw;
+  }
 `;
 
+const BottomWrapper =  styled.div`
+	height: 70px;
+	display: flex;
+	flex-direction: row;
+  justify-content: start;
+  background-color: rgba(0,0,0,0.5); 
+  align-items: center;
+`
+
+const Logo = styled.img`
+	height: 45px;
+	width: 45px;
+	margin: 10px; 
+`;
+
+const Title = styled.h2`
+	color: white;
+`;
 
 
 class Thumbnail extends Component {
 
 	render() {
 		return (
-			<div>
-				<Background />
-			</div>
+			<Background>
+				<BottomWrapper>
+					<Logo src={require('./assets/logo.png')}/>
+					<Title>Home and Away</Title>
+				</BottomWrapper>
+			</Background>
 		);
 	}
 }
